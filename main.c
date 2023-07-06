9e06e69936fd711bd4172a13bfec66fac814e0b6
Rusomati <mangenthegamer@gmail.com>
2023-07-06 07:52:51 +0000

    added the ability to read headers and tested the program to make sure that it works

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 //gets 256^x
 int getprefix(int power)
 {
@@ -25,18 +26,32 @@ int main()
 {
 	unsigned char a[128]={0};//for testing
 	FILE* fp = fopen("0083.bmp","rb");
+	int filesize=0;
 	if(fp == NULL)
 	{
 		fprintf(stderr,"cannot open file");
 		return -1;
 	}
-	unsigned char buffer[14]={0};
+	unsigned char headbuffer[6]={0};
 
-	fread(buffer,sizeof(buffer[0]),14,fp);
-	for(int i=0;i<14;i++)printf("%d\n",buffer[i]);
-	strncpy(a,buffer+2,4);//skips the first 2 bytes then reads 4 bytes from buffer then puts them into "a"
+	fread(headbuffer,sizeof(headbuffer[0]),6,fp);
+	//for(int i=0;i<6;i++)printf("%d\n",headbuffer[i]);
+	strncpy(a,headbuffer+2,4);//skips the first 2 bytes then reads 4 bytes from buffer then puts them into "a"
+	filesize = bytetoint(a,4);
+	unsigned char *buffer = malloc(sizeof(char) * filesize);
+	fread(buffer,sizeof(buffer[0]),(filesize-6),fp);
+	//for(int i=0;i<6;i++)printf("%d\n",buffer[i]);
+	//from here the code "byte_addres=byte_addres-6"
+
+	strncpy(a,buffer+4,4);//gets the starting address of the pizels
+	printf("%d\n",bytetoint(a,4));
+	strncpy(a,buffer+12,4);//gets the width of the image
+	printf("%d\n",bytetoint(a,4));
+	strncpy(a,buffer+16,4);//gets the height of the image//wrong according to the wiki
 	printf("%d\n",bytetoint(a,4));
 
+	free(buffer);
+	buffer = NULL;
 	fclose(fp);
 	return 0;
 }
