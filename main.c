638bc926110be7bdbcd73cfad4b4280e21851c3f
Rusomati <mangenthegamer@gmail.com>
2023-07-06 14:21:48 +0000

    made some progress that isnt usefull on its own

--- main.c
+++ main.c
@@ -22,6 +22,20 @@ int bytetoint(unsigned char *bytes,int length)
 	}
 	return res;
 }
+void getpixel(unsigned char *buffer,int firstpixel,int width,int height,int x,int y)
+{
+	y = height - y;//optimisable?
+	int rowsize=width+(width%4);//some magick because rows are padded
+	//pixels start from the bottom left or smth
+	int bytetoread = (3 * ((rowsize * y)+x))+firstpixel;
+	for(int i=0;i<rowsize*height;i++)
+	{
+	bytetoread = (3*i)+firstpixel;
+	//printf("index:\t\t%d\n",bytetoread-6);//?
+	printf("\033[48;2;%d;%d;%dm  \033[0m",buffer[bytetoread-4],buffer[bytetoread-5],buffer[bytetoread-6]);
+	if(!(i%rowsize))printf("\n");
+	}
+}
 int main()
 {
 	unsigned char tempbytes[4]={0};
@@ -41,12 +55,17 @@ int main()
 	fread(buffer,sizeof(buffer[0]),(filesize-6),fp);
 	//from here the code "byte_addres=byte_addres-6"
 
-	strncpy(tempbytes,buffer+4,4);//gets the starting address of the pizels
-	printf("%d\n",bytetoint(tempbytes,4));
-	strncpy(tempbytes,buffer+12,4);//gets the width of the image
-	printf("%d\n",bytetoint(tempbytes,4));
-	strncpy(tempbytes,buffer+16,4);//gets the height of the image
-	printf("%d\n",bytetoint(tempbytes,4));
+	strncpy(tempbytes,buffer+4,4);
+	int firstpixel = bytetoint(tempbytes,4);
+	//the starting address of the pixels
+	//should i add "-6" at the end?
+	//can i optimise this mess?
+	strncpy(tempbytes,buffer+12,4);//the width of the image
+	int width = bytetoint(tempbytes,4);
+	strncpy(tempbytes,buffer+16,4);//the height of the image
+	int height = bytetoint(tempbytes,4);
+	//printf("filesize:\t%d\n",filesize);//?
+	getpixel(buffer,firstpixel,width,height,0,0);//-6 here? can i mix the first 2 arguments then add -6?
 
 	free(buffer);
 	buffer = NULL;
