8533b85f552e2af46383b4eedbcb7e88829bf446
Rusomati <mangenthegamer@gmail.com>
2023-07-06 07:59:27 +0000

    removed dumb/irrelevant comments

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ int bytetoint(unsigned char *bytes,int length)
 }
 int main()
 {
-	unsigned char a[128]={0};//for testing
+	unsigned char tempbytes[4]={0};//for testing
 	FILE* fp = fopen("0083.bmp","rb");
 	int filesize=0;
 	if(fp == NULL)
@@ -35,20 +35,18 @@ int main()
 	unsigned char headbuffer[6]={0};
 
 	fread(headbuffer,sizeof(headbuffer[0]),6,fp);
-	//for(int i=0;i<6;i++)printf("%d\n",headbuffer[i]);
-	strncpy(a,headbuffer+2,4);//skips the first 2 bytes then reads 4 bytes from buffer then puts them into "a"
-	filesize = bytetoint(a,4);
+	strncpy(tempbytes,headbuffer+2,4);//skips the first 2 bytes then reads 4 bytes from buffer then puts them into "a"
+	filesize = bytetoint(tempbytes,4);
 	unsigned char *buffer = malloc(sizeof(char) * filesize);
 	fread(buffer,sizeof(buffer[0]),(filesize-6),fp);
-	//for(int i=0;i<6;i++)printf("%d\n",buffer[i]);
 	//from here the code "byte_addres=byte_addres-6"
 
-	strncpy(a,buffer+4,4);//gets the starting address of the pizels
-	printf("%d\n",bytetoint(a,4));
-	strncpy(a,buffer+12,4);//gets the width of the image
-	printf("%d\n",bytetoint(a,4));
-	strncpy(a,buffer+16,4);//gets the height of the image//wrong according to the wiki
-	printf("%d\n",bytetoint(a,4));
+	strncpy(tempbytes,buffer+4,4);//gets the starting address of the pizels
+	printf("%d\n",bytetoint(tempbytes,4));
+	strncpy(tempbytes,buffer+12,4);//gets the width of the image
+	printf("%d\n",bytetoint(tempbytes,4));
+	strncpy(tempbytes,buffer+16,4);//gets the height of the image
+	printf("%d\n",bytetoint(tempbytes,4));
 
 	free(buffer);
 	buffer = NULL;
